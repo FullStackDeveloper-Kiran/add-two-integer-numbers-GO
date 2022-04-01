ef5586e7da87da9cda0c14c96f21fc3fe353a9b9
Kiran M <94104935+FullStackDeveloper-Kiran@users.noreply.github.com>
2022-04-01 13:16:44 +0530

    Create add-two-numbers.go

---
+++ add-two-numbers.go
@@ -0,0 +1,16 @@
+//Golang program to add two integer numbers.
+
+package main
+import "fmt"
+
+func main() {
+    //Declare 3 integer type variables
+    var num1 int =10
+    var num2 int =20
+    var num3 int =0
+    
+    //Add num1,num2 and assign result to num3
+    num3=num1+num2
+    
+    fmt.Println("Addition is: ",num3)
+}
